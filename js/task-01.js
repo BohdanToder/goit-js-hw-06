4afea519701d64fab3904b66b7a08f7057b134e1
BohdanToder <bohdantoder7454@gmail.com>
2022-01-17 18:48:57 +0200

    Updates first task solution

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const allCategories = document.querySelectorAll('ul#categories > li.item');
 console.log(`Number of categories: ${allCategories.length}`);
 
 allCategories.forEach(element => {
-    console.log(`Category: ${element.firstElementChild.textContent}`)
+    console.log(`\nCategory: ${element.firstElementChild.textContent}`)
     const allCategoryElements = element.querySelectorAll('li');
     console.log(`Elements: ${allCategoryElements.length}`)
 });
