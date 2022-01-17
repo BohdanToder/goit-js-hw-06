3fcd3b9204e5d162a67b2751a817f75723f1fcfa
BohdanToder <bohdantoder7454@gmail.com>
2022-01-17 19:22:27 +0200

    Adds second task solution

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const mainList = document.querySelector('#ingredients');
+
+const mainListElements = ingredients.map(ingredient => {
+
+  const mainListElement = document.createElement('li');
+  mainListElement.textContent = ingredient;
+  mainListElement.classList.add('item');
+
+  return mainListElement;
+});
+
+mainList.append(...mainListElements);
