bcdb4fe095b11026217604a07aac177bd68913c1
BohdanToder <bohdantoder7454@gmail.com>
2022-01-19 09:44:51 +0200

    Adds eight task solution

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const inputEmail = document.querySelector('input[type=email]');
+const inputPassword = document.querySelector('input[type=password]');
+const formRef = document.querySelector('.login-form');
+
+formRef.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (inputEmail.value.length === 0 || inputPassword.value.length === 0) {
+        alert("Все поля должны быть заполнены!");
+    } else {
+        const inputValues = {
+            [inputEmail.name]: inputEmail.value,
+            [inputPassword.name]: inputPassword.value,
+        };
+        console.log(inputValues);
+        formRef.reset();
+    }
+});
 
