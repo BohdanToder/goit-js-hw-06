f4b5bb8139b0d72c0d9e2c0d10a249abcce7b4b4
BohdanToder <bohdantoder7454@gmail.com>
2022-01-19 16:42:04 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,20 @@
-const inputEmail = document.querySelector('input[type=email]');
-const inputPassword = document.querySelector('input[type=password]');
 const formRef = document.querySelector('.login-form');
 
 formRef.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (inputEmail.value.length === 0 || inputPassword.value.length === 0) {
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email === "" || password === "") {
         alert("Все поля должны быть заполнены!");
     } else {
         const inputValues = {
-            [inputEmail.name]: inputEmail.value,
-            [inputPassword.name]: inputPassword.value,
+            email,
+            password,
         };
-        console.log(inputValues);
-        formRef.reset();
-    }
+    console.log(inputValues);
+    formRef.reset();
+}
 });
-
