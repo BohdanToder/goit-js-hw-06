87ec61192659b6bf6118049d3da5cdf2acb19195
BohdanToder <bohdantoder7454@gmail.com>
2022-01-20 01:02:33 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,7 +3,7 @@ const helloName = document.querySelector('#name-output');
 const defaultName = "Anonymous";
 
 guestName.addEventListener("input", () => {
-    if (guestName.value != "") {
+    if (guestName.value !== "") {
         helloName.textContent = guestName.value;
     } else {
         helloName.textContent = defaultName;
