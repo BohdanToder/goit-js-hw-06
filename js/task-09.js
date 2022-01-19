43d96856934e563c5b8b01997c506fde6d583d33
BohdanToder <bohdantoder7454@gmail.com>
2022-01-19 10:05:59 +0200

    Adds ninth task solution

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const colorName = document.querySelector('.color');
+const buttonRef = document.querySelector('.change-color');
+
+function onButtonClick() {
+  const bgColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = bgColor;
+  colorName.textContent = bgColor;
+}
+
+buttonRef.addEventListener("click", onButtonClick);
